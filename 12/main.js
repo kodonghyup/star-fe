71a1447879dad0ede00dd63ee1305e0cefa27a8e
kodonghyup <donghyup1021@gamil.com>
2022-05-26 16:19:52 +0900

    12Week

---
+++ 12/main.js
@@ -0,0 +1,13 @@
+import avg from "./avg.js";
+import random from "./random.js";
+
+
+console.log(avg(1, 2, 3, 4));
+console.log(avg(1, 2));
+console.log(avg(1, 2, 3, 4, 5, 6));
+console.log(avg(1, 'a', 2));
+console.log(avg([1, 2, 3]));
+
+console.log(random(100, 200));
+console.log(random(1, 'sd'));
+console.log(random(5));
